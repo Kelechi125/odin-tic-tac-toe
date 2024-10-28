3f4918b0ef794507243ab436a89e40af55b81ff9
Kelechi125 <kelechi125@yahoo.com>
2024-10-28 08:55:04 -0500

    Printed array to become a grid in the console.

--- lib/board.rb
+++ lib/board.rb
@@ -2,11 +2,14 @@ def draw_board
   board_array = [["x1", "x2", "x3"], ["y1", "y2", "y3"], ["z1", "z2", "z3"]]
 
   board_array.each do |row|
-    row.each do |cell|
-      puts cell
+    row.each do |cell, index|
+      if index == 3
+        print cell
+      else
+        print cell + " |"
+      end
     end
-    
-    puts "--"
+    puts 
     
   end
 end
