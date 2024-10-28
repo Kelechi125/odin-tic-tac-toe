db56b92f3a8ee6eebbfc9607315e26cfbd1aacce
Kelechi125 <kelechi125@yahoo.com>
2024-10-28 08:59:36 -0500

    Changed row.each to row.each_with_index to properly access index of each cell and changed index == 3 to index == 2.

--- lib/board.rb
+++ lib/board.rb
@@ -2,8 +2,8 @@ def draw_board
   board_array = [["x1", "x2", "x3"], ["y1", "y2", "y3"], ["z1", "z2", "z3"]]
 
   board_array.each do |row|
-    row.each do |cell, index|
-      if index == 3
+    row.each_with_index do |cell, index|
+      if index == 2
         print cell
       else
         print cell + " |"
