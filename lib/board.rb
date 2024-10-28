fabddc84b0b5b84021653d5818f070205909e864
Kelechi125 <kelechi125@yahoo.com>
2024-10-28 09:42:55 -0500

    Created a Board class and moved the draw_board method inside the class. Instantiated a Board object.

--- lib/board.rb
+++ lib/board.rb
@@ -1,17 +1,21 @@
-def draw_board
-  board_array = [["x1", "x2", "x3"], ["y1", "y2", "y3"], ["z1", "z2", "z3"]]
-
-  board_array.each do |row|
-    row.each_with_index do |cell, index|
-      if index == 2
-        print cell
-      else
-        print cell + " |"
+class Board
+  def draw_board
+    board_array = [["x1", "x2", "x3"], ["y1", "y2", "y3"], ["z1", "z2", "z3"]]
+  
+    board_array.each do |row|
+      row.each_with_index do |cell, index|
+        if index == 2
+          print cell
+        else
+          print cell + " |"
+        end
       end
+      puts 
+      
     end
-    puts 
-    
   end
 end
 
-draw_board()
+ new_board = Board.new
+
+ new_board.draw_board
