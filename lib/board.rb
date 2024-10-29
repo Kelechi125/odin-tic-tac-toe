0ece67204276b7824187ad980ffd574baa72b5ce
Kelechi125 <kelechi125@yahoo.com>
2024-10-29 09:53:45 -0500

    Created horizontal divisions in the board.

--- lib/board.rb
+++ lib/board.rb
@@ -1,29 +1,23 @@
 class Board
   def draw_board
-=begin
+
     board_array = [["x1", "x2", "x3"], ["y1", "y2", "y3"], ["z1", "z2", "z3"]]
   
-    board_array.each do |row|
-      
-      row.each_with_index do |cell, index|
-        if index == 2
+    board_array.each_with_index do |row, index|
+      if index == 1 || index == 2
+        puts "_______________"
+      end
+      row.each_with_index do |cell, index_2|
+        if index_2 == 2
           print cell
         else
-          print cell + " |"
+          print cell + " | "
         end
         
       end
       puts 
-      
     end
-=end
   end
-
-  puts row = ["   " "|" "   " "|" "   "]
-  puts separator = "-----------"
-  puts row
-  puts separator
-  puts row
 end
 
 new_board = Board.new
