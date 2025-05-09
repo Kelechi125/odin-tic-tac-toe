caedcf6f9fddb298f61f6836be35571561e1bfcb
Kelechi125 <kelechi125@yahoo.com>
2025-05-09 16:45:34 -0500

    Update the board to use position numbers from 1 - 9 for empty spots.

--- lib/board.rb
+++ lib/board.rb
@@ -1,22 +1,35 @@
 class Board
+  attr_reader :board
+
   def initialize
     @board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
   end
 
   def draw_board
-    @board.each_with_index do |row, index|
-      if index > 0
+    counter = 1
+    @board.each_with_index do |row, row_index|
+      if row_index > 0
         puts "-----------"
       end
-      row.each_with_index do |cell, index_2|
-        if index_2 == 2
-          print cell
+      row.each_with_index do |cell, col_index|
+        display = cell == " " ? " #{counter}" : " #{cell}"
+        if col_index == 2
+          print display
         else
-          print cell + " | "
+          print display + " |"
         end
+        counter += 1
       end
       puts 
     end
+  end
+
+  def update_board(position, symbol)
+    puts "Please select a position number between 1 - 9."
+
+    row_index = (position - 1) / 3
+    col_index = (position - 1) % 3
+    @board[row_index][col_index] = symbol
   end
 end
 
