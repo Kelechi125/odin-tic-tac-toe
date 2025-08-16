48adbbe4f80082ebc46f5566b6f4fd8f8ae902ed
Kelechi125 <kelechi125@yahoo.com>
2025-08-16 11:33:06 -0500

    Moved game logic from player.rb to this file. Fixed the conditional statements.

---
+++ tic_tac_toe.rb
@@ -0,0 +1,48 @@
+require_relative 'lib/board'
+require_relative 'lib/player'
+
+puts "Player 1, please enter your name."
+name = gets.chomp
+player_one = Player.new(name)
+
+#Player 1 will select their symbol
+#Function will automatically assign Player 2's symbol
+#based on Player 1's selection
+def select_symbol()
+  letter = gets.chomp
+
+  if letter == "X" || letter == "x"
+    symbol_one = "X"
+    symbol_two = "O"
+  elsif letter == "O" || letter == "o"
+    symbol_one = "O"
+    symbol_two = "X"
+  elsif letter != "X" || letter != "x" || letter != "O" || letter != "o"
+    puts "Try again!"
+    return
+  end
+
+  #symbol_one is Player 1's symbol
+  #symbol_two is Player 2's symbol
+  return symbol_one, symbol_two
+end
+
+puts "Player 1, please select a symbol: X or O."
+
+select_symbol()
+
+puts "Player 2, please enter your name."
+name_two = gets.chomp
+player_two = Player.new(name_two)
+
+
+puts "Hello #{player_one.name} and #{player_two.name}! Let's start the game!"
+
+Board.new.draw_board
+
+# Need to figure out how to get the players to take turns
+# Need to figure out how to determine the winner
+# Maybe during each player's turn when they select a space, the update_board method
+# From the Board class get called
+# Each player has to select a number that indicates an empty space on the board
+# 
