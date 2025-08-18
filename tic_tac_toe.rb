2088e1371ac0c6fb1757e6fd2f68a9e000151b13
Kelechi125 <kelechi125@yahoo.com>
2025-08-17 20:47:11 -0500

    Trying to figure out how to prompt the program to ask for user input again if Player_One doesn't enter a proper selection.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -8,6 +8,8 @@ player_one = Player.new(name)
 #Player 1 will select their symbol
 #Function will automatically assign Player 2's symbol
 #based on Player 1's selection
+
+=begin
 def select_symbol()
   letter = gets.chomp
 
@@ -19,24 +21,50 @@ def select_symbol()
     symbol_two = "X"
   elsif letter != "X" || letter != "x" || letter != "O" || letter != "o"
     puts "Try again!"
-    return
   end
 
   #symbol_one is Player 1's symbol
   #symbol_two is Player 2's symbol
   return symbol_one, symbol_two
 end
+=end
 
-puts "Player 1, please select a symbol: X or O."
+loop do
+  puts "Player 1, please select a symbol: X or O."
 
-select_symbol()
+  symbol = gets.chomp
+
+  if symbol != "X" || symbol != "x" || symbol != "O" || symbol != "o"
+    puts "Try again!"
+  elsif symbol == "X" || symbol == "x"
+    symbol_one = "X"
+    symbol_two = "O"
+  elsif symbol == "O" || symbol == "o"
+    symbol_one = "O"
+    symbol_two = "X"
+  end
+
+  return symbol_one, symbol_two
+end
+
+
+
+# select_symbol()
+
+# I need to figure out how to handle when Player 1 puts anything other
+# than "X" or "O" as their symbol. I want the select_symbol method
+# to run itself over and over if anything other than these are typed
+# and for the method to stop if "X" or "O" are properly selected
+# (which would be the case most of the time)
 
 puts "Player 2, please enter your name."
 name_two = gets.chomp
 player_two = Player.new(name_two)
 
 
-puts "Hello #{player_one.name} and #{player_two.name}! Let's start the game!"
+puts "Hello #{player_one.name} and #{player_two.name}!"
+puts "#{player_one.name} is #{symbol_one}, and #{player_two.name} is #{symbol_two}."
+puts "Let's start the game!"
 
 Board.new.draw_board
 
