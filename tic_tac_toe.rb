32abfed0d9a5175d40ab0eb9894ce1a89a58d37a
Kelechi125 <kelechi125@yahoo.com>
2025-08-20 21:53:09 -0500

    Set up a very basic while loop for the purpose of simulating taking turns.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -31,8 +31,9 @@ end
 
 # assign_symbol returns an array from which I can pick out and assign symbols
 official_symbols = assign_symbol(symbol)
-symbol_one = official_symbols[0]
-symbol_two = official_symbols[1]
+
+symbol_one = official_symbols[0] #Player_One's symbol
+symbol_two = official_symbols[1] # Player_Two's symbol
 
 puts "Player 2, please enter your name."
 name_two = gets.chomp
@@ -51,13 +52,29 @@ Board.new.draw_board
 # Maybe during each player's turn when they select a space, the update_board method
 # From the Board class get called
 # Each player has to select a number that indicates an empty space on the board
-# Program might need to acount the number of each symbol present
+# Program might need to count the number of each symbol present
 # to determine whose turn it is currently
 # e.g If Player_One is "X" and Player_Two is "O",
 # If there's more "X's" than "O's" at any point,
 # it's Player_Two's turn. Otherwise if the amount
 # of "X's" and "O's" are equal, it's Player_One's turn
+# Could make a method within the Board class that
+# loops through the @board after every turn to count
+# each symbol and then invoke the funciton call within
+# the update_board method at the very end
 
 # Need to figure out how to determine the winner
 # Winning conditions need to be checked after every single turn
 
+turn = 0
+
+while turn < 9 do
+
+  puts "Player 1's turn"
+  Board.update_board()
+
+  puts "Player 2's turn"
+  Board.update_board()
+
+  turn += 1
+end
